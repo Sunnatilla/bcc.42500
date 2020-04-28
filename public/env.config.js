5a2cd3f2ad6a06a35da4c03b9eb41ef84219b7e8
shontykov <shontykov.kz@gmail.com>
2020-04-28 18:23:51 +0600

    env fix

--- public/env.config.js
+++ public/env.config.js
@@ -2,8 +2,8 @@ window.env = {
   OTP_URL: "https://wifi.bcc.kz",
   GREEN_API: "https://green.bcc.kz/camunda",
   REFERENCE_API: "https://green.bcc.kz/reference",
-  COLVIR_REST_URL: "http://10.20.52.96:5555",
-  APP_ID: "ceb82774-c2e5-4d6f-9978-dcbf7702c98b",
+  COLVIR_REST_URL: "https://prod-colvir-rest-green-front.apps.bcc.kz",
+  APP_ID: "c3a31fdb-9a46-4bcd-a4cb-ed8891d4c4ff",
   SERVICEBUS_XJSON_URL:
     "https://prod-reverse-cache-green-front.apps.bcc.kz/xjson?url=http://10.15.129.200:7080/services/",
 };
