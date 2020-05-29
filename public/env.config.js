a873a3074f18c0104da3cffe735256f365109c20
shontykov <shontykov.kz@gmail.com>
2020-05-29 08:23:20 +0600

    env to prod

--- public/env.config.js
+++ public/env.config.js
@@ -1,13 +1,13 @@
 window.env = {
   OTP_URL: "https://wifi.bcc.kz",
-  GREEN_API: "http://10.20.52.96:9001/inline/api",
-  REFERENCE_API: "http://10.20.52.96:5335",
-  COLVIR_REST_URL: "http://10.20.52.96:5555",
-  APP_ID: "ceb82774-c2e5-4d6f-9978-dcbf7702c98b",
-  CLIENT_INFO: "http://10.20.52.96:5002",
+  GREEN_API: "https://green.bcc.kz/camunda",
+  REFERENCE_API: "https://green.bcc.kz/reference",
+  COLVIR_REST_URL: "https://prod-colvir-rest-green-front.apps.bcc.kz",
+  APP_ID: "9f65923f-1864-46d8-9bac-13407aa36af4",
+  CLIENT_INFO: "https://prod-client-info-green-front.apps.bcc.kz",
   SERVICE_BUS: {
-    URL: "http://10.15.123.56:8801/v1/rps",
-    APP_ID: "f4d36029-edb1-4d73-97d9-d0ee6282c0",
+    URL: "http://10.15.129.200:8801/v1/rps",
+    APP_ID: "c274fb89-0d53-4304-95e9-130b267cd326",
   },
   PRODUCT: {
     SOCIAL: "0.300.114",
